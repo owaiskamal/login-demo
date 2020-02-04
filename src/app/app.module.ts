5eb1aa2f9254bbe8194d1c381116a2b7a699bfed
Usama Tariq <usama@cr-pl.com>
2020-02-04 17:12:43 +0500

    change-password-page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChangePasswordPageComponent } from './change-password-page/change-password-page.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {CheckboxModule} from 'primeng/checkbox';
+import {RadioButtonModule} from 'primeng/radiobutton'
+import {ButtonModule} from 'primeng/button';
+import {InputTextModule} from "primeng/inputtext";
+import {PasswordModule} from 'primeng/password';
+import {CardModule} from 'primeng/card';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChangePasswordPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    CheckboxModule,
+    RadioButtonModule,
+    ButtonModule,
+    InputTextModule,
+    PasswordModule,
+    CardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
