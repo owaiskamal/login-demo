7295a1c0f90afc0f5850769ad0390b69e0b425d6
Owais A. Khan <owais.khan@cr-pl.com>
2020-02-12 14:33:28 +0500

    servicemsg

--- src/app/login-page/login-page.component.ts
+++ src/app/login-page/login-page.component.ts
@@ -1,50 +1,42 @@
-import { Component, OnInit } from '@angular/core';
-import {MessageService} from 'primeng/api';
-import { Router } from '@angular/router';
-import { UserauthService } from '../userauth.service';
+import { Component, OnInit } from "@angular/core";
+import { MessageService } from "primeng/api";
+import { Router } from "@angular/router";
+import { UserauthService } from "../userauth.service";
 @Component({
-  selector: 'app-login-page',
-  templateUrl: './login-page.component.html',
-  styleUrls: ['./login-page.component.scss'],
+  selector: "app-login-page",
+  templateUrl: "./login-page.component.html",
+  styleUrls: ["./login-page.component.scss"]
 })
 export class LoginPageComponent implements OnInit {
+  constructor(
+    private messageService: MessageService,
+    private router: Router,
+    private authService: UserauthService
+  ) {}
 
-  constructor(private messageService :  MessageService , private router : Router , private authService : UserauthService) { }
+  userName: string;
+  password: string;
+  ngOnInit() {}
 
-  userName : string;
-  password : string;
-  ngOnInit() {
-  }
-
-  loginUser()
-  {
-    this.authService.userAuth(this.userName , this.password).subscribe(
-      res =>
-    {
-      console.log("user" , res)
-     var msg = res;
-      if( msg[0]["STATUS"]  ==  "0;Invalid User ID and Password")
-      {
+  loginUser() {
+    this.authService.userAuth(this.userName, this.password).subscribe(res => {
+      console.log("user", res);
+      var msg = res;
+      if (msg[0]["STATUS"] == "0;Invalid User ID and Password") {
         console.log("error");
+      } else {
+        this.messageService.add({
+          severity: "success",
+          summary: "Login Successfully",
+          detail: "Welcome User"
+        });
+        setTimeout(() => {
+          this.router.navigateByUrl("/adminpage");
+        }, 2000);
       }
-      else{
-      this.messageService.add
-      ({
-        severity:'success',
-        summary:'Login Successfully',
-        detail: 'Welcome User'
-      });
-      setTimeout(() => {
-        this.router.navigateByUrl('/adminpage');
-      }, 2000);
-    }
+    });
   }
-    )
-    
-      
-  }
-  changePassword()
-  {
-    this.router.navigateByUrl('/changepassword');
+  changePassword() {
+    this.router.navigateByUrl("/changepassword");
   }
 }
